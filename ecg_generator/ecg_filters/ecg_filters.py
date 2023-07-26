b4c574f6ca309558583a1714aaf35906345e19c7
tikhonp <tikhonp@users.noreply.github.com>
2023-07-26 16:48:46 +0300

    🐛 Fix filters exceptions

--- ecg_generator/ecg_filters/ecg_filters.py
+++ ecg_generator/ecg_filters/ecg_filters.py
@@ -8,8 +8,17 @@ from ecg_generator.sample_rate import SampleRate
 def process_data(data: np.ndarray, sample_rate: SampleRate) -> np.ndarray:
     data = data[data != 0]
     data = CompensationFilter(frequency=sample_rate.value).filter(data)
-    data = HighPass(sample_hz=418, cutoff_hz=1)(data)
-    data = LowPass(sample_hz=418, cutoff_hz=35)(data)
+    try:
+        data = HighPass(sample_hz=418, cutoff_hz=1)(data)
+    except:
+        pass
+    try:
+        data = LowPass(sample_hz=418, cutoff_hz=35)(data)
+    except:
+        pass
     # data = BandPass(sample_hz=418, lower_cutoff_hz=40, higher_cutoff_hz=60)(data)
-    data = LowPass(sample_hz=418, cutoff_hz=75)(data)
+    try:
+        data = LowPass(sample_hz=418, cutoff_hz=75)(data)
+    except:
+        pass
     return data

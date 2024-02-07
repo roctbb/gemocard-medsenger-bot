0705972e5612f1dc8efc00091942d901db844cf1
roctbb <roctbb@gmail.com>
2024-02-07 14:53:06 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 sudo pip3 install -r requirements.txt
+sudo cp gemocard.ini /etc/uwsgi/apps/
 sudo cp agents_gemocard.conf /etc/supervisor/conf.d/
 sudo cp agents_gemocard_nginx.conf /etc/nginx/sites-enabled/
 sudo supervisorctl update
